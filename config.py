c53e86b1ab7d0e4dfa0a946ba57e4df314526c61
b0om1987 <105589045+b0om1987@users.noreply.github.com>
2022-10-07 12:24:39 +0500

    Update config.py

--- config.py
+++ config.py
@@ -6,10 +6,10 @@ TOKEN = os.getenv('BOT_TOKEN')
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
-ADAPTABLE_APP_NAME = os.getenv('ADAPTABLE_APP_NAME')
+ADAPTABLE_APP_NAME = os.getenv('EXTERNAL_URL')
 
 # webhook settings
-WEBHOOK_HOST = f'https://{ADAPTABLE_APP_NAME}.adaptable.app'
+WEBHOOK_HOST = f'{ADAPTABLE_APP_NAME}'
 WEBHOOK_PATH = f'/webhook/{TOKEN}'
 WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
 
