7ff938df0d04e1d8e141038435ff3c337b5eb9c1
b0om1987 <105589045+b0om1987@users.noreply.github.com>
2022-10-07 16:44:33 +0500

    Update config.py

--- config.py
+++ config.py
@@ -15,4 +15,4 @@ WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
 
 # webserver settings
 WEBAPP_HOST = '0.0.0.0'
-WEBAPP_PORT = os.getenv('PORT', default=8000)
+WEBAPP_PORT = os.getenv('PORT', default=3000)
