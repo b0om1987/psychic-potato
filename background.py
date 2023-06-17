7edf644018d37c7d30aee2713a11bcefe8e2fb5c
b0om1987 <105589045+b0om1987@users.noreply.github.com>
2023-06-18 00:50:13 +0500

    Create background.py

---
+++ background.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask import request
+from threading import Thread
+import os
+import time
+import requests
+
+
+app = Flask('')
+
+@app.route('/')
+def home():
+  return "Sup, homie"
+
+def run():
+  app.run(host = '0.0.0.0', port = os.getenv('PORT', default=3000))
+
+def keep_alive():
+  t = Thread(target=run)
+  t.start()
